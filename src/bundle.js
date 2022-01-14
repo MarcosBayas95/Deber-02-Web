a5c54ca300d8b831e3f16fa74e4188a3765800b2
SneyderJLO <69530232+SneyderJLO@users.noreply.github.com>
2022-01-14 17:38:54 -0500

    pagina 2 - terminada (#9)
    * pagina 2 - formulario - validaciones - terminada
    
    pagina 2 terminada

--- src/bundle.js
+++ src/bundle.js
@@ -1,1 +1,5 @@
-(()=>{const e=document.querySelector("#bar"),t=document.querySelector("#nav");e.addEventListener("click",(()=>{t.classList.toggle("invisible"),t.classList.toggle("flex"),t.classList.toggle("h-0"),t.classList.toggle("opacity-0")}))})();
+(() => {
+    const e = document.querySelector("#bar"),
+        t = document.querySelector("#nav");
+    e.addEventListener("click", (() => { t.classList.toggle("invisible"), t.classList.toggle("flex"), t.classList.toggle("h-0"), t.classList.toggle("opacity-0") }))
+})();
